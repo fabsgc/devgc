1def0aac0285639937d43ad9ec9eede7cdcd8df8
Fabien Beaujean <fabienbeaudimi@hotmail.fr>
2016-01-03 17:23:44 +0000

    fucking missing files

---
+++ app/resource/request/Plugin/Captcha.php
@@ -0,0 +1,12 @@
+<?php
+	namespace Controller\Request\Plugin;
+	
+	/**
+	 * @property \System\Form\Validation\Validation $validation
+	*/
+	trait Captcha{
+		public function captcha(){
+			$this->validation->text('captcha', 'captcha')
+				->equal('coucou', 'Le captcha ne correspond pas à l\'image');
+		}
+	}
