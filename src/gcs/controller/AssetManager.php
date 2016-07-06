01e994eab2d23a032d107b985d953a1e844a488d
fabsgc <fabienbeaudimi@hotmail.fr>
2016-07-07 01:50:02 +0200

    add cache asset manager + https url

--- src/gcs/controller/AssetManager.php
+++ src/gcs/controller/AssetManager.php
@@ -18,7 +18,7 @@
 				Response::instance()->contentType("text/" . $_GET['type']);
 				Response::instance()->header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + 2592000));
 
-				$cache = new Cache(html_entity_decode($_GET['id'] . '.' . $_GET['type']), 0);
+				$cache = new Cache(html_entity_decode($_GET['id'] . '.' . $_GET['type']), Config::config()['user']['output']['asset']['cache']);
 
 				return $cache->getCache();
 			}
