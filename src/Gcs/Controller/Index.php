6fef8ca672c16e617118d9113c561bd4407949b5
fabsgc <fabienbeaudimi@hotmail.fr>
2017-05-06 14:44:03 +0200

    fix controller generation

--- src/Gcs/Controller/Index.php
+++ src/Gcs/Controller/Index.php
@@ -1,33 +1,33 @@
 <?php
-    namespace Src\Gcs\Controller;
+namespace Src\Gcs\Controller;
 
-    use Gcs\Framework\Core\Config\Config;
-    use Gcs\Framework\Core\Controller\Controller;
-    use Gcs\Framework\Core\Response\Response;
-    use Gcs\Framework\Core\Template\Template;
+use Gcs\Framework\Core\Config\Config;
+use Gcs\Framework\Core\Controller\Controller;
+use Gcs\Framework\Core\Response\Response;
+use Gcs\Framework\Core\Template\Template;
 
-	/**
-	 * Class Index
-	 * @package Gcs
-	 * @Before(class="\Src\Gcs\Controller\Index", method="init")
-	 * @After(class="\Src\Gcs\Controller\Index", method="end")
-	 */
+/**
+ * Class Index
+ * @package Gcs
+ * @Before(class="\Src\Gcs\Controller\Index", method="init")
+ * @After(class="\Src\Gcs\Controller\Index", method="end")
+ */
 
-	class Index extends Controller {
+class Index extends Controller {
 
-		public function init() {
-			if (Config::config()['user']['debug']['environment'] != 'development') {
-				Response::instance()->status(404);
-			}
-		}
+    public function init() {
+        if (Config::config()['user']['debug']['environment'] != 'development') {
+            Response::instance()->status(404);
+        }
+    }
 
-		/**
-		 * @Routing(name="index", url="(/*)", method="get,post,put")
-		 */
+    /**
+     * @Routing(name="index", url="(/*)", method="get,post,put")
+     */
 
-		public function actionDefault() {
-			return (new Template('index/default', 'gcsDefault'))
-				->assign('title', 'GCsystem V' . VERSION)
-				->show();
-		}
-	}
+    public function actionDefault() {
+        return (new Template('index/default', 'gcsDefault'))
+            ->assign('title', 'GCsystem V' . VERSION)
+            ->show();
+    }
+}
--- src/Gcs/Controller/Lang.php
+++ src/Gcs/Controller/Lang.php
@@ -1,30 +1,30 @@
 <?php
-	namespace Src\Gcs\Controller;
+namespace Src\Gcs\Controller;
 
-	use Gcs\Framework\Core\Config\Config;
-	use Gcs\Framework\Core\Controller\Controller;
-	use Gcs\Framework\Core\Response\Response;
+use Gcs\Framework\Core\Config\Config;
+use Gcs\Framework\Core\Controller\Controller;
+use Gcs\Framework\Core\Response\Response;
 
-	/**
-	 * Class Lang
-	 * @package Gcs
-	 * @Before(class="\Src\Gcs\Controller\Lang", method="init")
-	 */
+/**
+ * Class Lang
+ * @package Gcs
+ * @Before(class="\Src\Gcs\Controller\Lang", method="init")
+ */
 
-	class Lang extends Controller {
+class Lang extends Controller {
 
-		public function init() {
-			if (Config::config()['user']['debug']['environment'] != 'development') {
-				Response::instance()->status(404);
-			}
-		}
+    public function init() {
+        if (Config::config()['user']['debug']['environment'] != 'development') {
+            Response::instance()->status(404);
+        }
+    }
 
-		/**
-		 * @Routing(name="gcs.lang.default", url="/gcs/lang(/*)", method="get")
-		 * @return mixed
-		 */
+    /**
+     * @Routing(name="gcs.lang.default", url="/gcs/lang(/*)", method="get")
+     * @return mixed
+     */
 
-		public function actionDefault() {
-			return $this->showDefault();
-		}
-	}
+    public function actionDefault() {
+        return $this->showDefault();
+    }
+}
